3c01a3983a0c44df7d0cd0ea2959c612a4a85541
Chase Michael Emory <cemory@linux-lab-019.ece.uw.edu>
2019-03-15 11:01:51 -0700

    final version 64bits

--- bp_me/src/v/network/bp_network_deserializer.v
+++ bp_me/src/v/network/bp_network_deserializer.v
@@ -25,23 +25,30 @@ module bp_network_deserializer
   assign ready_o = 1'b1;
 
   logic [`BSG_SAFE_CLOG2(num_packets_p)-1:0] count [num_src-1:0];
+  logic fifo_en;
+  logic [src_id_width_p-1:0] p_addr;  
 
+  wire [src_id_width_p-1:0]                 curr_addr = data_i[(total_o_data_width-dest_id_width_p-1)-:src_id_width_p];
 
-  wire [src_id_width_p-1:0]                 curr_addr = data_i[(source_data_width_p-dest_id_width_p-1)-:src_id_width_p];
-  wire [packet_data_width_p-1:0]            curr_data = data_i[0+:packet_data_width_p]
+  wire [packet_data_width_p-1:0]            curr_data = data_i[0+:packet_data_width_p];
 
   wire [`BSG_SAFE_CLOG2(num_packets_p)-1:0] count_p1 = count[curr_addr] + 1'b1;
 
-  wire                                      fifo_en = v_i & (count_p1 == num_packets_p);
+  wire                                      fifo_en_r = v_i & (count_p1 == num_packets_p-1);
 
-  wire [num_packets_p-1:0]                  curr_mask = num_packets_p'b1 << count[curr_addr];
+  wire [num_packets_p-1:0]                  curr_mask = 1'b1 << count[curr_addr];
 
 
   wire [`BSG_SAFE_CLOG2(num_packets_p)-1:0] count_n = ( v_i ? ((count_p1 == num_packets_p) ? '0 : count_p1) : count[curr_addr] );
 
 
   always_ff @ (posedge clk_i) begin
-    if(reset_i) count <= '0;
+    if(reset_i) begin
+      integer i;
+      for(i = 0; i < num_src; i = i + 1) begin
+        count[i] <= '0;
+      end // for(i = 0; i < num_src; i = i + 1)
+    end // if(reset_i)
     else begin
       count[curr_addr] <= count_n;  
     end // else
@@ -50,47 +57,49 @@ module bp_network_deserializer
   wire fifo_vo;
   wire [src_id_width_p-1:0] data_addr_fifo;
 
-  bsg_fifo_1r1w_small queue
-    #(.width_p(src_id_width_p)
-    , .els_p(num_src)
-    , .ready_THEN_valid_p(0)
-    )
+  bsg_fifo_1r1w_small #(.width_p(src_id_width_p)
+                      , .els_p(num_src)
+                      , .ready_THEN_valid_p(0)
+                      )
+    queue
     ( .clk_i(clk_i)
     , .reset_i(reset_i)
 
     , .v_i(fifo_en)
-    , .ready_o(ready_o)
-    , .data_i(curr_addr)
+    , .ready_o()
+    , .data_i(p_addr)
 
     , .v_o(fifo_vo)
     , .data_o(data_addr_fifo)
-    , .yumi_i(yumi_i)
+    , .yumi_i(yumi_i & fifo_vo)
     );
 
-  bsg_mem_1r1w_sync_mask_write_var scratch_pad 
-    #(.width_p(1)
-    , .mask_width_p(packet_data_width_p)
-    , .els_p(num_src)
-    , .harden_p(1'b0)
-    )
+  wire [total_data_width-1:0] ram_data_out;
+
+  bsg_mem_1r1w_sync_mask_write_var #(.width_p(total_data_width)
+                                   , .mask_width_p(packet_data_width_p)
+                                   , .els_p(2**`BSG_SAFE_CLOG2(num_src))
+                                   , .harden_p(1'b0)
+                                   )
+    scratch_pad
     ( .clk_i(clk_i)
     , .reset_i(reset_i)
     , .w_v_i(v_i)
     , .w_mask_i(curr_mask)
     , .w_addr_i(curr_addr)
-    , .w_data_i(curr_data)
+    , .w_data_i({num_packets_p{curr_data}})
     , .r_v_i(1'b1)
     , .r_addr_i(data_addr_fifo)
-    , .r_data_o(data_o)
+    , .r_data_o(ram_data_out)
     );
 
   always_ff @(posedge clk_i) begin
     v_o <= fifo_vo;
+    fifo_en <= fifo_en_r;
+    p_addr <= curr_addr;
   end
 
-
-
-
+  assign data_o = ram_data_out[source_data_width_p-1:0];
 
 endmodule
 
